01c324f7d97f3d1704f3591cc558c1898384f088
Samuel <samuelb.conta@gmail.com>
2024-10-16 10:19:34 -0300

    Condições

---
+++ Módulo D/Condições/1main.js
@@ -0,0 +1,6 @@
+let vel = 78.3
+console.log(`A velocidade do carro é ${vel}Km/h`)
+if (vel > 60) { //condição simples, não tem else
+    console.log(`Você ultrapassou a velocidade permitida e será multado!`) 
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ Módulo D/Condições/2main.js
@@ -0,0 +1,7 @@
+let pais = 'EUA'
+console.log(`Morando em ${pais}`)
+if (pais != 'Brasil') {
+     console.log('Você é Estrangeiro')
+} else {
+    console.log('Você é Brasileiro')
+}
